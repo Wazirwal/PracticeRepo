9c54617d93ce58cea6b4b31fbea48394ef91fab3
gitpackage <noor@gmail.com>
2023-02-22 22:01:04 -0500

    Branching Practice

---
+++ src/Branching.java
@@ -0,0 +1,13 @@
+
+public class Branching {
+
+	public static void main(String[] args) {
+		
+	
+	 }
+
+	 public void iCanLearnGit() {
+		 System.out.println("I will keep trying and learn if");
+
+}
+}
